75919233373e6e402f098d0c96df40415c660332
B3rzxd3v <d3vilc0re@gmail.com>
2022-07-10 15:56:39 +0700

    Создали компоненты страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,27 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Welcome from '../../pages/welcome/welcome';
+/* import Login from '../../pages/login/login';
+import GameOver from '../../pages/gameOver/gameOver'; */
+/* import ArtistQuestions from '../../pages/artistQuestions/artistQuestions';
+import GenreQuestions from '../../pages/genreQuestions/genreQuestions';
+import WinScreen from '../../pages/winScreen/winScreen'; */
+/* import NotFound from '../../pages/notFound/notFound'; */
+
+type AppProps = {
+  errorsCount: number;
+}
+
+function App({errorsCount} : AppProps): JSX.Element {
+  return (
+    <>
+      <Welcome errorsCount={errorsCount} />
+      {/* <Login />
+      <GameOver /> */}
+      {/* <ArtistQuestions /> */}
+      {/* <GenreQuestions /> */}
+      {/* <WinScreen /> */}
+      {/* <NotFound /> */}
+    </>
+  );
 }
 
 export default App;
