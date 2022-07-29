900dcbfdf3506c4186c919820e59c97ea0c0782f
B3rzxd3v <d3vilc0re@gmail.com>
2022-07-29 18:52:22 +0700

    refactor game over screen & win screen

--- project/src/pages/winScreen/winScreen.tsx
+++ project/src/pages/winScreen/winScreen.tsx
@@ -1,6 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { resetGame } from '../../store/action';
 
 function WinScreen(): JSX.Element {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const {mistakes, step} = useAppSelector((state) => state);
+  const correctAnswers = step - mistakes;
+
   return (
     <section className="result">
       <div className="result-logout__wrapper">
@@ -8,8 +17,15 @@ function WinScreen(): JSX.Element {
       </div>
       <div className="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" /></div>
       <h2 className="result__title">Вы настоящий меломан!</h2>
-      <p className="result__total">Вы ответили правильно на 6 вопросов и совершили 2 ошибки</p>
-      <button className="replay" type="button">Сыграть ещё раз</button>
+      <p className="result__total">Вы ответили правильно на {correctAnswers} вопросов и совершили {mistakes} ошибки</p>
+      <button className="replay" type="button"
+        onClick={() => {
+          dispatch(resetGame());
+          navigate(AppRoute.Game);
+        }}
+      >
+        Сыграть ещё раз
+      </button>
     </section>
   );
 }
