9dbcd24b47ef594ebf42809f4496ac3c7e840e12
B3rzxd3v <d3vilc0re@gmail.com>
2022-07-29 17:25:47 +0700

    Добавим компонент для отображения ошибки

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import ErrorMessage from './components/error-message/error-message';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -12,6 +13,7 @@ root.render(
   <React.StrictMode>
     {/* устанавливаем глобальный store */}
     <Provider store={store}>
+      <ErrorMessage />
       <App />
     </Provider>
   </React.StrictMode>,
