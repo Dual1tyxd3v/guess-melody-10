d96e6e3f43cf516ac5cb37afa0e0a669d6383e4c
B3rzxd3v <d3vilc0re@gmail.com>
2022-07-29 12:28:31 +0700

    Подключили redux thunk

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -1,4 +1,15 @@
 import { reducer } from './reducer';
 import { configureStore } from '@reduxjs/toolkit';
+import { createAPI } from '../services/api';
+
+const api = createAPI();
 // инициализируем store передав редюсер
-export const store = configureStore({reducer});
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      }
+    }),
+});
