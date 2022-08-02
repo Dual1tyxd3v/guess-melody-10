e06694be79d108e1f005706636307f89a8b6a1c0
B3rzxd3v <d3vilc0re@gmail.com>
2022-08-02 11:24:56 +0700

    Добавили хук userAnswers

---
+++ project/src/hooks/use-user-answers.ts
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { QuestionGenre } from '../types/question';
+
+type ResultUserAnswers = [boolean[], (id: number, value: boolean) => void];
+
+export const useUserAnswers = (question: QuestionGenre): ResultUserAnswers => {
+  const answersCount = question.answers.length;
+
+  const [answers, setAnswers] = useState(Array.from({length: answersCount}, () => false));
+
+  const handleUserAnswers = (id: number, value: boolean) => {
+    const userAnswers = answers.slice();
+    userAnswers[id] = value;
+    setAnswers(userAnswers);
+  };
+
+  return [answers, handleUserAnswers];
+};
